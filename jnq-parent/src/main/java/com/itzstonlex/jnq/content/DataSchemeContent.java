af41994dddd2db877cb23ede04910c9f96a5e3cc
itzstonlex <seltixfromhell@bk.ru>
2022-08-01 03:23:39 +0400

    fill DataSchemeContent

--- jnq-parent/src/main/java/com/itzstonlex/jnq/content/DataSchemeContent.java
+++ jnq-parent/src/main/java/com/itzstonlex/jnq/content/DataSchemeContent.java
@@ -1,34 +1,74 @@
 package com.itzstonlex.jnq.content;
 
 import com.itzstonlex.jnq.DataConnection;
+import com.itzstonlex.jnq.field.impl.IndexDataField;
 import lombok.AccessLevel;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
-@Data
+@RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class DataSchemeContent {
 
+    Map<String, DataTableContent> tableByNamesMap = new ConcurrentHashMap<>();
+
+    @Getter
+    String name;
+
+    @Getter
     DataConnection connection;
 
-    Map<String, DataTableContent> tables;
-
-    public Set<String> getTablesNames() {
-        return tables == null ? Collections.emptySet() : tables.values().stream().map(DataTableContent::getName).collect(Collectors.toSet());
+    public @NonNull Set<DataTableContent> getTablesContents() {
+        return new HashSet<>(tableByNamesMap.values());
     }
 
-    public void create(@NonNull DataTableContent table) {
-        connection.
+    public @NonNull Set<String> getTablesNames() {
+        return tableByNamesMap.values().stream().map(DataTableContent::getName).collect(Collectors.toSet());
     }
 
-    public void drop(@NonNull DataTableContent table) {
+    public DataTableContent getTableContent(String name) {
+        return tableByNamesMap.get(name.toLowerCase());
+    }
 
+    public @NonNull CompletableFuture<Void> create(@NonNull DataTableContent table, @NonNull IndexDataField... fields) {
+        return table.create(fields).thenAccept(unused -> tableByNamesMap.put(table.getName().toLowerCase(), table));
+    }
+
+    public @NonNull CompletableFuture<Void> create() {
+        return connection.createRequest(this).factory()
+                .newCreateScheme()
+                .withExistsChecking()
+                .complete()
+                .updateAsync();
+    }
+
+    public @NonNull CompletableFuture<Void> clear(@NonNull DataTableContent table) {
+        return table.clear();
+    }
+
+    public @NonNull CompletableFuture<Void> clear() {
+        Set<CompletableFuture<Void>> completableFutureSet = new HashSet<>();
+
+        tableByNamesMap.forEach((name, dataTableContent) -> {
+
+            CompletableFuture<Void> future = dataTableContent.drop();
+            completableFutureSet.add(future);
+        });
+
+        return CompletableFuture.allOf(completableFutureSet.toArray(new CompletableFuture[0]));
+    }
+
+    public @NonNull CompletableFuture<Void> drop(@NonNull DataTableContent table) {
+        return table.drop().thenAccept(unused -> tableByNamesMap.remove(table.getName().toLowerCase()));
     }
 
 }
