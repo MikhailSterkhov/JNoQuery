54d6fb0c9193e7445dd51af2a1d971281173b34d
itzstonlex <seltixfromhell@bk.ru>
2022-08-28 20:38:48 +0400

    remove unusable FieldType`s

--- jnq-parent/src/main/java/com/itzstonlex/jnq/field/FieldType.java
+++ jnq-parent/src/main/java/com/itzstonlex/jnq/field/FieldType.java
@@ -19,30 +19,23 @@ public enum FieldType {
     INT(JDBCType.INTEGER.getVendorTypeNumber(), "INT", Integer.class),
     BIGINT(JDBCType.BIT.getVendorTypeNumber(), "BIGINT", Long.class),
     BIT(JDBCType.BIT.getVendorTypeNumber(), "BIT", Integer.class),
-    BOOLEAN(JDBCType.BOOLEAN.getVendorTypeNumber(), "BOOLEAN", Boolean.class),
 
     FLOAT(JDBCType.FLOAT.getVendorTypeNumber(), "FLOAT", Float.class),
     DOUBLE(JDBCType.DOUBLE.getVendorTypeNumber(), "DOUBLE", Double.class),
     LONG(JDBCType.BIGINT.getVendorTypeNumber(), "LONG", Long.class),
-    DECIMAL(JDBCType.DECIMAL.getVendorTypeNumber(), "DECIMAL", Integer.class),
+
+    BOOLEAN(JDBCType.BOOLEAN.getVendorTypeNumber(), "BOOLEAN", Boolean.class),
 
     CHAR(JDBCType.CHAR.getVendorTypeNumber(), "CHAR", Character.class),
     VARCHAR(JDBCType.VARCHAR.getVendorTypeNumber(), "VARCHAR", String.class),
-    TINYTEXT(JDBCType.VARCHAR.getVendorTypeNumber(), "TINYTEXT", CharSequence.class),
-    TEXT(JDBCType.VARCHAR.getVendorTypeNumber(), "TEXT", String.class),
-    MEDIUM_TEXT(JDBCType.NVARCHAR.getVendorTypeNumber(), "MEDIUMTEXT", CharSequence.class),
     LONGTEXT(JDBCType.LONGNVARCHAR.getVendorTypeNumber(), "LONGTEXT", String.class),
 
-    BINARY(JDBCType.BINARY.getVendorTypeNumber(), "BINARY", byte[].class),
-    VAR_BINARY(JDBCType.VARBINARY.getVendorTypeNumber(), "VARBINARY", byte[].class),
-    TINY_BLOB(JDBCType.BLOB.getVendorTypeNumber(), "TINYBLOB", Blob.class),
-    BLOB(JDBCType.BLOB.getVendorTypeNumber(), "BLOB", Blob.class),
-    MEDIUM_BLOB(JDBCType.BLOB.getVendorTypeNumber(), "MEDIUMBLOB", Blob.class),
-    LONG_BLOB(JDBCType.BLOB.getVendorTypeNumber(), "LONGBLOB", Blob.class),
+    BLOB(JDBCType.BLOB.getVendorTypeNumber(), "BLOB", byte[].class),
+    MEDIUMBLOB(JDBCType.BLOB.getVendorTypeNumber(), "LONGBLOB", Byte[].class),
+    LONGBLOB(JDBCType.BLOB.getVendorTypeNumber(), "LONGBLOB", Blob.class),
 
     DATE(JDBCType.DATE.getVendorTypeNumber(), "DATE", Date.class),
     TIME(JDBCType.TIME.getVendorTypeNumber(), "TIME", Time.class),
-    DATETIME(JDBCType.DATE.getVendorTypeNumber(), "DATETIME", Date.class),
     TIMESTAMP(JDBCType.TIMESTAMP.getVendorTypeNumber(), "TIMESTAMP", Timestamp.class),
 
     OBJECT(JDBCType.JAVA_OBJECT.getVendorTypeNumber(), "UNKNOWN", Object.class),
